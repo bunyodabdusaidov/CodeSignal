11f915bc4aa71adfa4dee5a9108ace2eb27146b7
bunyodabdusaidov <bunyodabdusaidov0@gmail.com>
2021-12-04 21:01:13 -0600

    the algorithm works generally, but it doesn't work for complex inputs. it should be checked with long inputs and complex inputs

--- Intro/3. Smooth Sailing/reverseInParentheses.py
+++ Intro/3. Smooth Sailing/reverseInParentheses.py
@@ -1,33 +1,60 @@
-def solution(inputString):
-    output = extract_paren(inputString)
-
-    print(output)
-
 def extract_paren(inputString):
     output = ""
     parens = []
     l = 0
     r = 0
     
-    for i in range(len(inputString)):
+    i = 0
+    while '(' or ')' in inputString:
+        #print(f"i = {i}")
         if inputString[i] == '(':
-            parens.append(i)
+            parens = [i]
+            #print(f"`(` = {parens[0]}")
         if inputString[i] == ')':
             parens.append(i)
-    i = 0
-    while i < len(parens):
-        org = inputString[:parens[i]]
-        print(f"org: {org}")
-        rev = inputString[parens[i]+1:parens[i+1]][::-1]
-        print(f"rev: {rev}")
-
-        output += org + rev
-        print(f"output: {output}")
-        i += 2
-
+            rev_word = inputString[parens[0]+1:parens[1]]
+            #print(f"`)` = {parens[1]}")
+            #print(f"parens = {parens[0]}, {parens[1]}")
+            #print(parens)
+            #print(f"parens: {inputString[parens[0]+1:parens[1]]}")
+            #print(f"inputString[:parens[0]] = {inputString[:parens[0]]}")
+            #print(f"inputString[parens[1]+1:] = {inputString[parens[1]+1:]}")
+            inputString = inputString[:parens[0]] + rev_word[::-1] + inputString[parens[1]+1:]
+            #print(f"inputString: {inputString}")
+            i = 0
+        i += 1
+        if i >= len(inputString):
+            break
     
-    return output
+    return inputString
 
-inputString =  "foo(bar)baz(blim)"
-solution(inputString)
+def solution(inputString):
+    parens = []
+    
+    i = 0
+
+    if inputString == "":
+        return inputString
+
+    while '(' or ')' in inputString:
+        if inputString[i] == '(':
+            parens = [i]
+        if inputString[i] == ')':
+            parens.append(i)
+            
+            begin = inputString[:parens[0]]
+            rev_word = inputString[parens[0]+1:parens[1]]
+            end = inputString[parens[1]+1:]
+            inputString = begin + rev_word[::-1] + end
+            i = 0
+        
+        i += 1
+        if i >= len(inputString):
+            break
+    
+    return inputString
+
+inputString = "(foo(bar(baz)))blim(go)(yes(no(maybe)))"
+print(inputString)
+print(solution(inputString))
 
