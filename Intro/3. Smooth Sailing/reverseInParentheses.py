d8161cc82b9f276f973660dcedc18120cfe3a312
bunyodabdusaidov <bunyodabdusaidov0@gmail.com>
2021-12-06 10:24:40 -0600

    problem solved with adding two if statements to check for an empty string and only parentheses, and changing while loop statement from  to

--- Intro/3. Smooth Sailing/reverseInParentheses.py
+++ Intro/3. Smooth Sailing/reverseInParentheses.py
@@ -1,60 +1,43 @@
-def extract_paren(inputString):
-    output = ""
-    parens = []
-    l = 0
-    r = 0
-    
-    i = 0
-    while '(' or ')' in inputString:
-        #print(f"i = {i}")
-        if inputString[i] == '(':
-            parens = [i]
-            #print(f"`(` = {parens[0]}")
-        if inputString[i] == ')':
-            parens.append(i)
-            rev_word = inputString[parens[0]+1:parens[1]]
-            #print(f"`)` = {parens[1]}")
-            #print(f"parens = {parens[0]}, {parens[1]}")
-            #print(parens)
-            #print(f"parens: {inputString[parens[0]+1:parens[1]]}")
-            #print(f"inputString[:parens[0]] = {inputString[:parens[0]]}")
-            #print(f"inputString[parens[1]+1:] = {inputString[parens[1]+1:]}")
-            inputString = inputString[:parens[0]] + rev_word[::-1] + inputString[parens[1]+1:]
-            #print(f"inputString: {inputString}")
-            i = 0
-        i += 1
-        if i >= len(inputString):
-            break
-    
-    return inputString
-
 def solution(inputString):
     parens = []
-    
     i = 0
-
-    if inputString == "":
+    
+    # if there are no parentheses, return the input itself
+    if '(' and ')' not in inputString:
         return inputString
 
-    while '(' or ')' in inputString:
-        if inputString[i] == '(':
-            parens = [i]
-        if inputString[i] == ')':
-            parens.append(i)
-            
-            begin = inputString[:parens[0]]
-            rev_word = inputString[parens[0]+1:parens[1]]
-            end = inputString[parens[1]+1:]
-            inputString = begin + rev_word[::-1] + end
-            i = 0
-        
-        i += 1
-        if i >= len(inputString):
-            break
+    # else if input consists of only '(' and ')', return an empty string
+    elif '(' and ')' in inputString and len(inputString) == 2:
+        return ""
+
+    # else, loop through the string, and reverse strings in parentheses
+    else:
+        # repeat until there are no parentheses left
+        while '(' and ')' in inputString:
+            # add matching parentheses to `parens` list
+            if inputString[i] == '(':
+                parens = [i]
+            if inputString[i] == ')':
+                parens.append(i)
+                
+                # get the beginning of original string
+                begin = inputString[:parens[0]]
+                # get the word in parentheses
+                rev_word = inputString[parens[0]+1:parens[1]]
+                # get the remaining string
+                end = inputString[parens[1]+1:]
+                # concatinate beginning, reversed word, and end of string
+                inputString = begin + rev_word[::-1] + end
+                # set i = 0 to start the loop again and check for any parentheses
+                i = 0
+            # if parentheses not found, increment i
+            else:
+                i += 1
+                # if i reaches the end of string, break the loop
+                if i >= len(inputString):
+                    break
     
     return inputString
 
-inputString = "(foo(bar(baz)))blim(go)(yes(no(maybe)))"
-print(inputString)
-print(solution(inputString))
-
+#inputString = "foo(barzab)blim"
+#print(solution(inputString))
